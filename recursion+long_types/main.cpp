4be82273fe45bdb1372b68a77a29ed2a23100388
Ari Archer <asnojus039@gmail.com>
2021-08-03 23:01:06 +0300

    update @ Tue  3 Aug 23:01:06 EEST 2021

--- recursion+long_types/main.cpp
+++ recursion+long_types/main.cpp
@@ -1,14 +1,11 @@
 #include <iostream>
 
 unsigned long long int fib(unsigned long long int x) {
-    if (x <= 1) {
-        return x;
-    } else {
-        return fib(x - 1) + fib(x - 2);
-    }
+    if (x <= 1) return x;
+    return fib(x - 1) + fib(x - 2);
 }
 
 int main() {
-    std::cout << "Fibonacci 25: " << fib(25) << "\n";
+    std::cout << "Fibonacci 25 O(n): " << fib(25) << "\n";
     return 0;
 }
